3685a155f54b29c65aed4c9fff1f7782a7f56a3c
Fenny <fenny@fennyfatal.com>
2017-11-28 11:54:38 -0700

    Add method to get podcast series.

--- lib/google_music_api/podcast.rb
+++ lib/google_music_api/podcast.rb
@@ -6,6 +6,27 @@ module GoogleMusicApi
     include Util
     include Track
     
+    #Get podcast episodes from series
+    #@id [string] id of the podcast series
+    #@num [int] count of episodes to grab
+    #@return [Array] of Hashes that describe episodes in a series
+    def get_podcast_series(id,num=25) 
+      url = 'podcast/fetchseries'
+ 
+      options = {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        query: {
+          'alt': 'json',
+          'nid': id,
+          'num': num
+        }
+      }
+
+      make_get_request url, options
+    end
+
     #Gets podcast episode metadata
     #@id [string] podcast id
     #@return [Array] of Hashes that describe a playlist
